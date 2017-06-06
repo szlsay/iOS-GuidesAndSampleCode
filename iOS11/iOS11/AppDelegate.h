d492d38bb0e8374ca07a04789d7d189dac224096
沈兆良 <409178030@qq.com>
2017-06-06 10:03:04 +0800

    添加iOS110

---
+++ iOS11/iOS11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS11
+//
+//  Created by ST on 17/6/6.
+//  Copyright © 2017年 ST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
