5125badae2589016cc77124b3317f1f29dcdb4d8
沈兆良 <409178030@qq.com>
2017-06-17 09:59:06 +0800

    完成第三章的swift3支持

--- 精通Swift设计模式/Chapter 03/Tuples.playground/section-1.swift
+++ 精通Swift设计模式/Chapter 03/Tuples.playground/section-1.swift
@@ -3,11 +3,11 @@ import Foundation;
 var myProduct = ("Kayak", "A boat for one person", "Watersports", 275.0, 10);
 
 func writeProductDetails(product: (String, String, String, Double, Int)) {
-    println("Name: \(product.0)");
-    println("Description: \(product.1)");
-    println("Category: \(product.2)");
+    print("Name: \(product.0)");
+    print("Description: \(product.1)");
+    print("Category: \(product.2)");
     let formattedPrice = NSString(format: "$%.2lf", product.3);
-    println("Price: \(formattedPrice)");
+    print("Price: \(formattedPrice)");
 }
 
-writeProductDetails(myProduct);
+writeProductDetails(product: myProduct);
